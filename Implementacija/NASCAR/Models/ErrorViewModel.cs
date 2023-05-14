7b2b39c01490f7bd9420b69f62cbd625d035f606
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-05-14 22:26:27 +0200

    initial setup

---
+++ Implementacija/NASCAR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NASCAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
