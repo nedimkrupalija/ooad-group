1458a0debb32ae916eee408e6f3b32c19e69a38b
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-05-28 17:27:20 +0200

    Added db context

--- Implementacija/NASCAR/Data/ApplicationDbContext.cs
+++ Implementacija/NASCAR/Data/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
+using NASCAR.Models
 namespace NASCAR.Data
 {
     public class ApplicationDbContext : IdentityDbContext
@@ -17,10 +17,14 @@ namespace NASCAR.Data
             modelBuilder.Entity<User>().ToTable("User")
                 .HasDiscriminator<string>("UserType")
                 .HasValue<RegisteredUser>("RegisteredUser")
-                .HasValue<Admin>("Admin")
+                .HasValue<Admin>("Admin");
             modelBuilder.Entity<Vehicle>().ToTable("Vehicle");
             modelBuilder.Entity<Reservation>().ToTable("Reservation");
-
+            modelBuilder.Entity<Address>().ToTable("Address");
+            modelBuilder.Entity<CardDetails>().ToTable("CardDetails");
+            modelBuilder.Entity<Discount>().ToTable("Discount");
+            modelBuilder.Entity<DriversLicence>().ToTable("DriversLicence");
+            
             base.OnModelCreating(modelBuilder);
         }
 
--- Implementacija/NASCAR/Models/Vehicle.cs
+++ Implementacija/NASCAR/Models/Vehicle.cs
@@ -1,22 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NASCAR.Models
 {
     public class Vehicle
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public int ModelYear { get; set; }
-        public bool IsReserved { get; set; }
-        public TransmissionEnum Transmission { get; set; }
-        public double Mileage { get; set; }
-        public CategoryEnum Category { get; set; }
-        public string Description { get; set; }
-        public string Picutre { get; set; }
-        public string Color { get; set; }
-        public int Seats { get; set; }
-        public int Doors { get; set; }
+        public string? Name { get; set; }
+        public double? Price { get; set; }
+        public int? ModelYear { get; set; }
+        public bool? IsReserved { get; set; }
+        public TransmissionEnum? Transmission { get; set; }
+        public double? Mileage { get; set; }
+        public CategoryEnum? Category { get; set; }
+        public string? Description { get; set; }
+        public string? Picutre { get; set; }
+        public string? Color { get; set; }
+        public int? Seats { get; set; }
+        public int? Doors { get; set; }
 
         public Vehicle()
         {
