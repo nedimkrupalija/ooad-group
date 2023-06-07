962c6edab3e8e8225a6ba1c58153f97b33501151
nedimkrupalija <krupalijanedim@gmail.com>
2023-06-07 11:13:14 +0200

    Added about page

---
+++ Implementacija/NASCAR/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NASCAR.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
