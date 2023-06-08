226cd155c184756698a21dac0fc83de2d61e0391
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2023-06-08 16:12:47 +0200

    Mig

--- Implementacija/NASCAR/Controllers/VehiclesController.cs
+++ Implementacija/NASCAR/Controllers/VehiclesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using NASCAR.Data;
 using NASCAR.Models;
 
---
+++ Implementacija/NASCAR/wwwroot/js/reservation.js
@@ -0,0 +1,33 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    $("#end_date").form - control({
+        onSelect: function () {
+            (this).change();
+        }
+    });
+
+    $("#start_date").form - control({
+        onSelect: function () {
+            $(this).change();
+        }
+    });
+
+    $('#start_date').on('change', function () {
+        alert('Element ' + this.name + 'clicked');
+        if (Model.DropDate != null && Model.PickUpDate > Model.DropDate) {
+            let date1 = new DateTime(Model.PickUpDate);
+            let date2 = new DateTime(Model.DropDate);
+
+            Model.price = Math.ceil((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24)) * Model.Vehicle.Price;
+        }
+    });
+
+    $('#end_date').on('change', function () {
+        if (Model.PickUpDate != null && Model.PickUpDate > Model.DropDate) {
+            let date1 = new DateTime(Model.PickUpDate);
+            let date2 = new DateTime(Model.DropDate);
+
+            Model.price = Math.ceil((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24)) * Model.Vehicle.Price;
+        }
+    });
+});
