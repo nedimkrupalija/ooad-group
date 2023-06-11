99fb61131c5c18ccd3c801f03ff2795d3a778fe9
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-06-11 19:25:19 +0200

    mailfix

--- Implementacija/NASCAR/Services/AuthMessageSenderOptions.cs
+++ Implementacija/NASCAR/Services/AuthMessageSenderOptions.cs
@@ -2,6 +2,6 @@
 {
     public class AuthMessageSenderOptions
     {
-        public string? SendGridKey { get; set; }
+        public string? SendGridKey { get; set; } = "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";
     }
 }
