bab2a9c944a24e688b1aa382b7698d2e3308e950
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2023-06-11 04:22:18 +0200

    creational

--- Implementacija/NASCAR/Code/FactoryDiscount.cs
+++ Implementacija/NASCAR/Code/FactoryDiscount.cs
@@ -1,6 +1,58 @@
-﻿namespace NASCAR.Code
+﻿
+namespace NASCAR.Code
+
 {
-    public class Factory
+    public interface IDiscount
     {
+        double calculateDiscount(int days);
     }
+
+    public class WeeklyDiscount : IDiscount 
+    {
+
+        double IDiscount.calculateDiscount(int days)
+        {
+            return 0.1;
+        }
+    }
+    public class SuperDiscount : IDiscount
+    {
+        double IDiscount.calculateDiscount(int days)
+        {
+            return 0.2;
+        }
+    }
+
+    public class MaxDiscount : IDiscount 
+    {
+        double IDiscount.calculateDiscount(int days)
+        {
+            return 0.3;
+        }
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract IDiscount GetDiscount(string name);
+    }
+
+    public class ConcreteDiscountFactory : DiscountFactory
+    {
+        public override IDiscount GetDiscount(string name)
+        {
+            switch (name)
+            {
+                case "Weekly":
+                    return new WeeklyDiscount();
+                case "Super":
+                    return new SuperDiscount();
+                case "Max":
+                    return new MaxDiscount();
+                default:
+                    throw new ApplicationException(string.Format("Vehicle '{0}' cannot be created", name));
+            }
+        }
+
+    }
+
 }
